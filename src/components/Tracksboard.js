edadb75db1dd69429e05cc26cfb5a0b0ccc5874f
Jonathan Luquet <jonathanluquet@Jonathans-iMac.local>
2021-04-29 17:11:37 +0200

    tweeks css

--- src/components/Tracksboard.js
+++ src/components/Tracksboard.js
@@ -7,18 +7,23 @@ const closeTracksboard = () => {
 const Tracksboard = (props) => {
   return (
     <div className="Tracksboard hidden" id="tracksboard">
-      <div className="closeBtn" onClick={closeTracksboard}>
-        <div>Tracks en commun</div>
-        <div>X</div>
+      <div className="trackslist">
+        <div className="tracksboard-title" onClick={closeTracksboard}>
+          <div>Tracks en commun</div>
+          <div className="closebtn">Fermer X</div>
+        </div>
+        {props.matchedTracks.map((track) => {
+          return (
+            <div key={track.id} className="track-container">
+              <div className="artist">{track.artist}</div>
+              <div classname="track">
+                {track.track} ({track.popularity})
+              </div>
+            </div>
+          );
+        })}
+        <div>Total : {props.matchedTracks.length}</div>
       </div>
-      {props.matchedTracks.map((track) => {
-        return (
-          <div key={track.id} className="track-container">
-            <div className="artist">{track.artist}</div>
-            <div classname="track">{track.track}</div>
-          </div>
-        );
-      })}
     </div>
   );
 };
