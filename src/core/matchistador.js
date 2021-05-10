70cdf22b4a49b1fe6721cbb1e22f4577766be3a4
Jonathan Luquet <jonathanluquet@Jonathans-iMac.local>
2021-05-10 19:57:23 +0200

    remove fetch jsonp again

--- src/core/matchistador.js
+++ src/core/matchistador.js
@@ -1,5 +1,3 @@
-import fetchJsonp from 'fetch-jsonp';
-
 let front_url = 'https://www.matchistador.com';
 let api_url = 'https://api.matchistador.com';
 //
@@ -81,29 +79,37 @@ const matchistador = {
     console.log('authProcess');
 
     //récupération du token de deezer avec le code récupéré
-    try {
-      const response = await fetchJsonp(
+
+    const response = await fetch(
+      matchistador.corsAnywhereUrl +
         `https://connect.deezer.com/oauth/access_token.php?app_id=${encodeURIComponent(
           matchistador.deezer_clientId
         )}&secret=${encodeURIComponent(
           matchistador.deezer_clientSecret
-        )}&code=${encodeURIComponent(code)}&response_type=token&output=jsonp`
-      );
-
-      const deezerData = await response.json();
-
-      if (deezerData.access_token) {
-        localStorage.setItem('access_token', deezerData.access_token);
-        console.log('access token reçu de deezer');
-        await matchistador.getMyInfoFromDeezer();
-        localStorage.setItem('platform', 'deezer');
+        )}&code=${encodeURIComponent(code)}&response_type=token&output=json`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       }
-      if (localStorage.getItem('access_token')) {
-        await matchistador.registerMe();
-      }
-    } catch (error) {
-      console.error(error);
+    );
+
+    const deezerData = await response.json();
+
+    if (deezerData.access_token) {
+      localStorage.setItem('access_token', deezerData.access_token);
+      console.log('access token reçu de deezer');
+      await matchistador.getMyInfoFromDeezer();
+      localStorage.setItem('platform', 'deezer');
     }
+    if (localStorage.getItem('access_token')) {
+      await matchistador.registerMe();
+    }
+
+    // if (localStorage.getItem('access_token')) {
+    //   // await matchistador.registerMe();
+    // }
   },
 
   registerMe: async () => {
